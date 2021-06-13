34998a11dcbc4f75a0613fd3147902b9625f471a
Aamir dammad <wolfstreetmack@gmail.com>
2021-06-13 16:31:16 +0530

    database name update

--- NeighborlyAPI/updateAdvertisement/__init__.py
+++ NeighborlyAPI/updateAdvertisement/__init__.py
@@ -12,7 +12,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             # DONE: Update with appropriate MongoDB connection information
             url = "mongodb://neighborly-db-account:t2r7hpz8NXdu5ekhd7MruazEG8D5GAtbcOmMTKRlzxm9S9E3CBtOY8ZroTIszz6SRMNKUDLp7oo20ABnpNjnGA==@neighborly-db-account.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborly-db-account@"
             client = pymongo.MongoClient(url)
-            database = client['azure']
+            database = client['neighborlydb']
             collection = database['advertisements']
             
             filter_query = {'_id': ObjectId(id)}
