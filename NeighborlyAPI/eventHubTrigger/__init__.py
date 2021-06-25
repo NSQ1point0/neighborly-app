eed9db9ac632cca842346b1cf81e05eb468a1952
Aamir dammad <wolfstreetmack@gmail.com>
2021-06-25 23:21:24 +0530

    update

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -21,5 +21,22 @@ def main(event: func.EventGridEvent):
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
 
+def main(req: func.HttpRequest, sendGridMessage: func.Out[str]) -> func.HttpResponse:
+
+    value = "Sending message from my Azure Functions HTTP Trigger"
+
+    message = {
+        "personalizations": [ {
+          "to": [{
+            "email": "dammadfirstnameaamir@gmail.com"
+            }]}],
+        "subject": "[AZURE FUNCTIONS SENDGRID] email",
+        "content": [{
+            "type": "text/plain",
+            "value": value }]}
+
+    sendGridMessage.set(json.dumps(message))
+    return func.HttpResponse("Message successfully sent.")
+
 
 
